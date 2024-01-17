cdc6641952c1b4ed62b2c7d99e3a5c929567594b
Aryan021312 <139524607+Aryan021312@users.noreply.github.com>
2024-01-18 01:08:52 +0545

    Additional Info
    Sign In

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const port = process.env.PORT||7070;
 
 const app = express();
+var path=require('path');
 
-
-app.set('view engine', 'ejs');
+app.set('view engine', 'hbs');
 app.set('views', './views');
-app.use(express.static('./public'));
+app.use(express.static(path.join(__dirname,'public')));
 
 //use separate express router
 app.use('/',require('./routes'));
